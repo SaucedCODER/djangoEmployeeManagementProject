3d4146753977871ba1c8262e464bc918554d772f
unknown <zemigz007@gmail.com>
2023-11-08 23:56:33 +0800

    adding some customization i nthe admin part

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 # Register your models here.
+# remove group table
+admin.site.unregister(Group)
+
+admin.site.site_title = "CTinnovation"
+admin.site.site_header = "CivileTech Innovation"
+admin.site.index_title = "Admin Panel"
